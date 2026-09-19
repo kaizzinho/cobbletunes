bc4928209ef96fa13ad51eae8a9ca42045890568
arthurc0sme <cosmearthur14@gmail.com>
2026-09-19 15:45:55 -0300

    override fix

--- src/client/kotlin/com/kaizzinho/cobbletunes/client/compat/fancymenu/FancyMenuMusicSuppressor.kt
+++ src/client/kotlin/com/kaizzinho/cobbletunes/client/compat/fancymenu/FancyMenuMusicSuppressor.kt
@@ -2,6 +2,7 @@ package com.kaizzinho.cobbletunes.client.compat.fancymenu
 
 import com.kaizzinho.cobbletunes.MOD_ID
 import com.kaizzinho.cobbletunes.client.config.CobbleTunesClientConfig
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents
 import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.sound.SoundInstanceListener
@@ -19,25 +20,26 @@ object FancyMenuMusicSuppressor {
         debugLog: (String) -> Unit
     ) {
         if (registered || !available) return
-
-        val client = MinecraftClient.getInstance()
-        client.soundManager.registerListener(SoundInstanceListener { sound, _, _ ->
-            val shouldSuppress = config.replaceMenuMusic &&
-                client.world == null &&
-                sound.category == SoundCategory.MUSIC &&
-                sound.id.namespace != MOD_ID
-
-            if (shouldSuppress) {
-                client.soundManager.stop(sound)
-                if (loggedSoundIds.add(sound.id.toString())) {
-                    debugLog("[FancyMenu] suppressed menu music: ${sound.id}")
-                }
-            }
-        })
-
         registered = true
-        silenceExistingMenuMusic(client, config, debugLog)
-        debugLog("[FancyMenu] menu music suppression active")
+
+        ClientLifecycleEvents.CLIENT_STARTED.register { client ->
+            client.soundManager.registerListener(SoundInstanceListener { sound, _, _ ->
+                val shouldSuppress = config.replaceMenuMusic &&
+                    client.world == null &&
+                    sound.category == SoundCategory.MUSIC &&
+                    sound.id.namespace != MOD_ID
+
+                if (shouldSuppress) {
+                    client.soundManager.stop(sound)
+                    if (loggedSoundIds.add(sound.id.toString())) {
+                        debugLog("[FancyMenu] suppressed menu music: ${sound.id}")
+                    }
+                }
+            })
+
+            silenceExistingMenuMusic(client, config, debugLog)
+            debugLog("[FancyMenu] menu music suppression active")
+        }
     }
 
     fun silenceExistingMenuMusic(
